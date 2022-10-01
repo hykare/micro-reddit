f72601f67e8e6756b8f1b9342cd32a870385d6c9
hykare <78385964+hykare@users.noreply.github.com>
2022-10-01 21:05:30 +0200

    associations

--- db/migrate/20220929185748_create_comments.rb
+++ db/migrate/20220929185748_create_comments.rb
@@ -2,8 +2,9 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :body
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :post, null: false, foreign_key: true
 
       t.timestamps
     end
