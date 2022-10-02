07c07780669de1d4bd6671b21a2c91cd19492795
hykare <78385964+hykare@users.noreply.github.com>
2022-10-02 15:24:39 +0200

    post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, :body, presence: true
+  validates :title, length: { maximum: 100 }
+  validates :body, length: { maximum: 2000 }
 end
